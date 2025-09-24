ba9de793c416f45b8ebdfa307a3bb902f5a11fc4
sp4ce14 <aw136061@gmail.com>
2025-09-24 11:28:32 +0500

    routing

--- BookStoreApp/src/app/app-routing.module.ts
+++ BookStoreApp/src/app/app-routing.module.ts
@@ -1,28 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { RouterModule, Routes} from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
-import { RouterModule, Routes} from '@angular/router';
+
 const routes : Routes = [
   {path:'about-us', component: AboutUsComponent},
   {path:'how-it-works', component: HowItWorksComponent}
 ]
+
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutUsComponent,
-    HowItWorksComponent
-  ],
   imports: [
-    BrowserModule,
     RouterModule.forRoot(routes)
   ],
   exports: [
     RouterModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ]
 })
-export class AppModule { }
+export class AppRoutingModule { }
