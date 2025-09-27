27ff57e805e65861b5cb0b01aca338d3b6094aaf
sp4ce14 <aw136061@gmail.com>
2025-09-27 14:26:50 +0500

    random

--- BookStoreApp/src/app/shared/shared.module.ts
+++ BookStoreApp/src/app/shared/shared.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { RouterModule } from '@angular/router';
-
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
-    NotFoundComponent
+    NotFoundComponent,
+    ToolbarComponent
   ],
   imports: [
-    RouterModule
-  ]
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports: [ToolbarComponent]
 })
 export class SharedModule { }
