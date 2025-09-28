8eef55976a1a1959f3a497316f82c70108805c79
sp4ce14 <aw136061@gmail.com>
2025-09-28 15:14:02 +0500

    removeddummycode

--- BookStoreApp/src/app/auth/components/login/login.component.ts
+++ BookStoreApp/src/app/auth/components/login/login.component.ts
@@ -1,30 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  public name: string = '';
-  public email: string = '';
-  public id: number = 0;
+export class LoginComponent {
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
-
-  ngOnInit(): void {
-    this.route.queryParams.subscribe(param => {
-      this.name = param['name'];
-      this.email = param['email'];
-      this.id = param['id'];
-    })
-  }
-  goToBookDetails(idi: number): void {
-    this.router.navigate(['/public/book-details', idi], { queryParams: { name: 'ahmed' } })
-  }
-  goToSignup(): void {
-    this.router.navigate(['auth/signup'])
-  }
 
 }
