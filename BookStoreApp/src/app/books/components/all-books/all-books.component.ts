8ab281c22e413acb0de9c30bc0706523d0f59bb4
sp4ce14 <aw136061@gmail.com>
2025-09-29 22:27:12 +0500

    getset

--- BookStoreApp/src/app/books/components/all-books/all-books.component.ts
+++ BookStoreApp/src/app/books/components/all-books/all-books.component.ts
@@ -9,10 +9,20 @@ import { BookModel } from '../../models/book.model';
 })
 export class AllBooksComponent implements OnInit {
   public books: BookModel[] = [];
+
   constructor(public bookService: BookService) { }
+
   ngOnInit(): void {
-    this.books = this.bookService.getBooks();
-    console.log(this.books);
+    const allBooks: BookModel[] = this.bookService.getBooks();
+    allBooks.forEach((b: BookModel) => {
+      var obj = new BookModel(); 
+        obj.id = b.id,
+        obj.totalPages = b.totalPages,
+        obj.title = b.title,
+        obj.price = b.price,
+        obj.author = b.author
+        this.books.push(obj); 
+    })
   }
 
 }
--- BookStoreApp/src/app/books/models/book.model.ts
+++ BookStoreApp/src/app/books/models/book.model.ts
@@ -1,9 +1,16 @@
 import { PriceModel } from "./price.model";
 
-export interface BookModel {
-    id: number,
-    title: string,
-    totalPages: number,
-    author: string,
-    price: PriceModel
+export class BookModel {
+    public id: number = 0;
+    public title: string = '';
+    public totalPages: number = 0;
+    private _author: string = '';
+    public price: PriceModel = {currency: '', value: 0};
+
+    public set author(value: string){
+        this._author = value;
+    }
+    public get author(){
+        return ('Author: '+ this._author);
+    }
 }
